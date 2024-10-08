2142bd5a03ec7bd80522198be5e9565714e7d185
nicki <nicolas_cernadas@hotmail.com>
2024-10-08 15:55:14 -0300

    Agrega la precision (oscila entre %37 y %40

--- testing_analisis.py
+++ testing_analisis.py
@@ -66,19 +66,17 @@ df = limpieza_col(df)
 # ====================================== ERROR^2 =========================================== #
 
 # Prueba con una línea dada
-m = 1.93939
-b = 4.73333
-sum_of_squares = 0.0
-df_copy = df[['ph', 'Potability']].copy()
-# calcular la suma de cuadrados
-for p in df_copy.itertuples(index=False):
-    y_salida = p[1]
-    y_predict = m*p[0] + b
-    residual_squared = (y_predict - y_salida)**2
-    sum_of_squares += residual_squared
-print(f"suma de cuadrados = {sum_of_squares}")
-
-
+# m = 1.93939
+# b = 4.73333
+# sum_of_squares = 0.0
+# df_copy = df[['ph', 'Potability']].copy()
+# # calcular la suma de cuadrados
+# for p in df_copy.itertuples(index=False):
+#     y_salida = p[1]
+#     y_predict = m*p[0] + b
+#     residual_squared = (y_predict - y_salida)**2
+#     sum_of_squares += residual_squared
+# print(f"suma de cuadrados = {sum_of_squares}")
 
 # ========================= GRAFICOS POST LIMPIEZA ============================ #
 # Son horribles
@@ -121,7 +119,48 @@ print(f"suma de cuadrados = {sum_of_squares}")
 # plt.title('Matriz de Correlacion')
 # plt.show()
 
-# ======================================================================================= #
+# ============================================== RED NEURONAL ========================================= #
 
+from sklearn.model_selection import train_test_split
+
+# Extraigo las columnas de entrada
+inputs = df.iloc[:, 0:9].values
+outputs = df.iloc[:, -1].values
+
+# Conjuntos de entrenamiento y prueba
+x_train, x_test, y_train, y_test = train_test_split(inputs, outputs, test_size=1/3)
+
+#shape retorna una tupla con las dimensiones de la matriz = (filas, columnas).
+# por lo que shape[0], nos retorna las filas de la matriz.
+n = x_train.shape[0] # número de registros de entrenamiento
+
+# Red neuronal
+# pesos
+w_hidden_1 = np.random.rand(3,9)
+w_output_1 = np.random.rand(1,3)
+
+# Como seria para ponerle 2 capas de neuronas?
+# w_hidden_2 = np.random.rand(,)
+# w_output_2 = np.random.rand(,)
+
+# sesgos
+b_hidden = np.random.rand(3,1)
+b_output = np.random.rand(1,1)
+
+# Funciones de Activacion
+relu = lambda x: np.maximum(x, 0)
+sigmoide = lambda x: 1 / (1 + np.exp(-x))
+
+def f_prop(X):
+    z1 = w_hidden_1 @ X + b_hidden
+    a1 = relu(z1)
+    z2 = w_output_1 @ a1 + b_output
+    a2 = sigmoide(z2)
+    return z1, a1, z2, a2
+
+test_predictions = f_prop(x_test.transpose())[3] # me interesa solo la capa de salida, A2
+test_comparisons = np.equal((test_predictions >= .5).flatten().astype(int), y_test)
+accuracy = sum(test_comparisons.astype(int) / x_test.shape[0])
+print("ACCURACY: ", accuracy)
 
 
