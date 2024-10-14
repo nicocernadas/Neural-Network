b48d682d09be6681510f9342cb7582678408a823
nicki <nicolas_cernadas@hotmail.com>
2024-10-14 13:24:36 -0300

    Back propagation y desc gradiente

--- testing_red.py
+++ testing_red.py
@@ -52,6 +52,53 @@ def f_prop(X):
     a2 = sigmoide(z2)
     return z1, a1, z2, a2
 
+# Derivadas de las funciones de activación
+d_relu = lambda x: x > 0
+d_sigmoide = lambda x: np.exp(-x) / (1 + np.exp(-x)) ** 2
+
+# Devuelve pendientes para pesos y sesgos
+# usando la regla de la cadena
+def b_prop(z1, a1, z2, a2, X, Y):
+    dC_dA2 = 2 * a2 - 2 * Y
+    dA2_dZ2 = d_sigmoide(z2)
+    dZ2_dA1 = w_output_1
+    dZ2_dW2 = a1
+    dZ2_dB2 = 1
+    dA1_dZ1 = d_relu(z1)
+    dZ1_dW1 = X
+    dZ1_dB1 = 1
+
+    dC_dW2 = dC_dA2 @ dA2_dZ2 @ dZ2_dW2.T
+    dC_dB2 = dC_dA2 @ dA2_dZ2 * dZ2_dB2
+    dC_dA1 = dC_dA2 @ dA2_dZ2 @ dZ2_dA1
+    dC_dW1 = dC_dA1 @ dA1_dZ1 @ dZ1_dW1.T
+    dC_dB1 = dC_dA1 @ dA1_dZ1 * dZ1_dB1
+
+    return dC_dW1, dC_dB1, dC_dW2, dC_dB2
+
+L = .05
+# Ejecutar descenso de gradiente
+for i in range(1_000_000):
+    # seleccionar aleatoriamente uno de los datos de entrenamiento
+    idx = np.random.choice(n, 1, replace=False)
+    X_sample = x_train[idx].transpose()
+    Y_sample = y_train[idx]
+
+    # pasar datos seleccionados aleatoriamente a través de la red neuronal
+    Z1, A1, Z2, A2 = f_prop(X_sample)
+
+    # distribuir error a través de la retropropagación
+    # y devolver pendientes para pesos y sesgos
+    dW1, dB1, dW2, dB2 = b_prop(Z1, A1, Z2, A2, X_sample, Y_sample)
+
+    # actualizar pesos y sesgos
+    w_hidden_1 -= L * dW1
+    b_hidden -= L * dB1
+    w_output_1 -= L * dW2
+    b_output -= L * dB2
+
+
+# Accuracy
 test_predictions = f_prop(x_test.transpose())[3] # me interesa solo la capa de salida, A2
 test_comparisons = np.equal((test_predictions >= .5).flatten().astype(int), y_test)
 accuracy = sum(test_comparisons.astype(int) / x_test.shape[0])
