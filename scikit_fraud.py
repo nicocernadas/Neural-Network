21802a1a06e66039df855135f0fe029733cb11ef
Nicolas Cernadas <nicolas_cernadas@hotmail.com>
2024-11-15 14:32:18 -0300

    Scikit Learn Neural Network, for comparison purposes

---
+++ scikit_fraud.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn import preprocessing
+from script_funciones import estandarizacion, limpieza
+
+df = pd.read_csv('./csvs/transf_data.csv', sep=',')
+
+df = limpieza(df)
+df = estandarizacion(df)[0]
+
+X = df.values[:, :-1]
+Y = df.values[:, -1]
+
+# Separo entrenamiento y prueba
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=1/3)
+
+nn = MLPClassifier(
+    solver='sgd',          
+    hidden_layer_sizes=(4,),
+    activation='relu',
+    max_iter=15_000,
+    learning_rate_init=.03,
+    random_state=42 # Semilla. Siempre pone los mismos pesos al principio, y divide la red igual. Es para mejor visualizacion
+)
+
+nn.fit(X_train, Y_train)
+
+print("Puntaje del conjunto de entrenamiento: %f" % nn.score(X_train, Y_train))
+print("Puntaje del conjunto de prueba: %f" % nn.score(X_test, Y_test))
